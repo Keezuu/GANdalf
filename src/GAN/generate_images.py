512dfe49ce3566d4d0333fd57794abaacaf61c31
Daniel Matwicki <jkeezuz@gmail.com>
2019-05-22 16:34:07 +0200

    Added constants to the code

--- src/GAN/generate_images.py
+++ src/GAN/generate_images.py
@@ -3,31 +3,21 @@ import torch
 from torch.autograd import Variable
 import numpy as np
 from torchvision.utils import save_image
-
+from src.resources import constants as cnst
 from src.GAN.Generator import Generator
 import matplotlib.pyplot as plt
 
 
-latent_size = 64
-hidden_size = 256
-image_size = 28*28
-batch_size = 64
-sample_dir = 'samples'
-chckpt_dir = 'save'
-save_dir = "data/GAN"
-
-
-
 
 # Create directories if they don't exist
-if not os.path.exists(sample_dir):
-    os.makedirs(sample_dir)
+if not os.path.exists(cnst.GAN_SAMPLES_DIR):
+    os.makedirs(cnst.GAN_SAMPLES_DIR)
 
-if not os.path.exists(save_dir):
-    os.makedirs(save_dir)
+if not os.path.exists(cnst.GAN_SAVE_DIR):
+    os.makedirs(cnst.GAN_SAVE_DIR)
 
 # Import MNIST dataset
-G = Generator(img_shape=(28, 28), n_classes=10, latent_dim=latent_size).cuda()
+G = Generator(img_shape=(28, 28), n_classes=10, latent_dim=cnst.GAN_LATENT_SIZE).cuda()
 FloatTensor = torch.cuda.FloatTensor
 LongTensor = torch.cuda.LongTensor
 
@@ -35,24 +25,25 @@ G.load_state_dict(torch.load('G.ckpt'))
 
 n_class = 10
 # Generate 1000 images from class "i" and save them in npy file
-z = Variable(FloatTensor(np.random.normal(0, 1, (10000, latent_size))))
+z = Variable(FloatTensor(np.random.normal(0, 1, (10000, cnst.GAN_LATENT_SIZE))))
 # Get labels ranging from 0 to n_classes for n rows
 labels = np.array([num for _ in range(1000) for num in range(n_class)])
 
 labels = Variable(LongTensor(labels))
 gen_imgs = G(z, labels)
 save_image(gen_imgs.reshape(gen_imgs.shape[0], 1, gen_imgs.shape[1], gen_imgs.shape[2]).data,
-           "samples/generatedVIS.png", nrow=n_class, normalize=True)
+           os.path.join(cnst.GAN_SAMPLES_DIR, "generatedVIS.png"), nrow=n_class, normalize=True)
 save_image(gen_imgs.reshape(gen_imgs.shape[0], 1, gen_imgs.shape[1], gen_imgs.shape[2]).data,
-           "samples/generatedVIS2.png", nrow=n_class*10, normalize=True)
+           os.path.join(cnst.GAN_SAMPLES_DIR, "generatedVIS.png"), nrow=n_class*10, normalize=True)
 gen_imgs = gen_imgs.cpu().data.numpy()
 labels = labels.cpu().data.numpy()
 # Move the data to CPU and then copy it to numpy array
-np.save(file=os.path.join(save_dir, "gan_images.npy"), arr=gen_imgs)
-np.save(file=os.path.join(save_dir, "gan_labels.npy"), arr=labels)
+np.save(file=os.path.join(cnst.GAN_DATA_DIR, "gan_images.npy"), arr=gen_imgs)
+np.save(file=os.path.join(cnst.GAN_DATA_DIR, "gan_labels.npy"), arr=labels)
 
 #visualize
-img_array = np.load(os.path.join(save_dir, "gan_images.npy"))
+img_array = np.load(os.path.join(cnst.GAN_DATA_DIR, "gan_images.npy"))
+
 for i in range(10):
     plt.title('Label is {label}'.format(label=i))
     plt.imshow(img_array[i], cmap='gray')
