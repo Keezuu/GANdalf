ab3b526da880fa56c584e20707a09b083f3b16ad
Daniel Matwicki <jkeezuz@gmail.com>
2019-07-12 18:20:31 +0200

    Updated generator architecture

--- src/GAN/Generator.py
+++ src/GAN/Generator.py
@@ -2,7 +2,7 @@ from fastai.imports import torch
 from torch import nn
 import numpy as np
 from src.resources import constants as cnst
-from sklearn.preprocessing import OneHotEncoder
+
 
 # DCGAN architecture based on pytorch DCGAN tutorial
 class Generator(nn.Module):
@@ -10,33 +10,29 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
 
         self.model = nn.Sequential(
-            nn.ConvTranspose2d(latent_dim, 8*cnst.GAN_GEN_FEATURE_MAPS,
-                               kernel_size=4, stride=1, padding=0, bias=False),
-            nn.BatchNorm2d(cnst.GAN_GEN_FEATURE_MAPS*8),
+            nn.ConvTranspose2d(latent_dim, 8 * cnst.GAN_GEN_FEATURE_MAPS,
+                               kernel_size=3, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(cnst.GAN_GEN_FEATURE_MAPS * 8),
             nn.ReLU(True),
-            # state size. (cnst.GAN_GEN_FEATURE_MAPS*8) x 4 x 4
+            # state size. (cnst.GAN_GEN_FEATURE_MAPS*8) x 3 x 3
             nn.ConvTranspose2d(cnst.GAN_GEN_FEATURE_MAPS * 8, cnst.GAN_GEN_FEATURE_MAPS * 4,
-                               4, 2, 1, bias=False),
+                               3, 2, 0, bias=False),
             nn.BatchNorm2d(cnst.GAN_GEN_FEATURE_MAPS * 4),
             nn.ReLU(True),
-            # state size. (cnst.GAN_GEN_FEATURE_MAPS*4) x 8 x 8
+            # state size. (cnst.GAN_GEN_FEATURE_MAPS*4) x 7 x 7
             nn.ConvTranspose2d(cnst.GAN_GEN_FEATURE_MAPS * 4, cnst.GAN_GEN_FEATURE_MAPS * 2,
                                4, 2, 1, bias=False),
             nn.BatchNorm2d(cnst.GAN_GEN_FEATURE_MAPS * 2),
             nn.ReLU(True),
-            # state size. (cnst.GAN_GEN_FEATURE_MAPS*2) x 16 x 16
-            nn.ConvTranspose2d(cnst.GAN_GEN_FEATURE_MAPS*2, cnst.GAN_GEN_FEATURE_MAPS,
+            # state size. (cnst.GAN_GEN_FEATURE_MAPS*2) x 14 x 14
+            nn.ConvTranspose2d(cnst.GAN_GEN_FEATURE_MAPS * 2, cnst.CHANNELS_NUM,
                                4, 2, 1, bias=False),
             nn.BatchNorm2d(cnst.GAN_GEN_FEATURE_MAPS),
             nn.ReLU(True),
-            # state size. (cnst.GAN_GEN_FEATURE_MAPS) x 32 x 32
+            # state size. (cnst.CHANNELS_NUM) x 28 x 28
             # Output is number of channels, for BW images its 1
-            nn.ConvTranspose2d(cnst.GAN_GEN_FEATURE_MAPS, cnst.CHANNELS_NUM,
-                               4, 2, 1, bias=False),
             nn.Tanh()
-            # state size. (cnst.CHANNELS_NUM) x 64 x 64
         )
-
 
         # def block(in_chann, out_chann):
         #     layers = [nn.Linear(in_chann, out_chann),
@@ -53,8 +49,6 @@ class Generator(nn.Module):
         #     nn.Tanh()
         # )
 
-
-
     def forward(self, noise):
         # Concatenate label embedding and image to produce input
         # transformed = self.label_emb.transform(labels.reshape(-1, 1)).toarray()
@@ -63,5 +57,5 @@ class Generator(nn.Module):
         # gen_input = torch.cat((embedded_tensor, noise), 1)
         # img = self.model(gen_input)
         img = self.model(noise)
-        #img = img.view(img.size(0), *self.img_shape)
+        # img = img.view(img.size(0), *self.img_shape)
         return img
