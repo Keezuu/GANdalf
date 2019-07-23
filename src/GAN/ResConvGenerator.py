d70cb61f44b39bcac44d5b62803d810515b28bd5
Daniel Matwicki <jkeezuz@gmail.com>
2019-07-23 19:05:05 +0200

    Added preprocess method to resconvgenerator as well

--- src/GAN/ResConvGenerator.py
+++ src/GAN/ResConvGenerator.py
@@ -1,19 +1,17 @@
 from fastai.imports import torch
 from torch import nn
-import numpy as np
 from src.resources import constants as cnst
 
 
-# DCGAN architecture based on pytorch DCGAN tutorial
 class ResConvGenerator(nn.Module):
-    def __init__(self, n_classes, latent_dim, img_shape):
+    def __init__(self, n_classes, latent_dim):
         super(ResConvGenerator, self).__init__()
 
         self.label_embedding = nn.Embedding(n_classes, n_classes)
 
         self.model = nn.Sequential(
             # input size is latent_dim
-            nn.Upsample(scale_factor=2, mode='bilinear'),
+            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
             nn.ReflectionPad2d(1),
             nn.Conv2d(latent_dim + n_classes, 8 * cnst.GAN_GEN_FEATURE_MAPS,
                       kernel_size=2, stride=1, padding=0),
@@ -21,7 +19,7 @@ class ResConvGenerator(nn.Module):
             nn.ReLU(True),
             # out size. (cnst.GAN_GEN_FEATURE_MAPS*8) x 3 x 3
             # ---------
-            nn.Upsample(scale_factor=2, mode='bilinear'),
+            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
             nn.ReflectionPad2d(1),
             nn.Conv2d(8 * cnst.GAN_GEN_FEATURE_MAPS, 4 * cnst.GAN_GEN_FEATURE_MAPS,
                       kernel_size=2, stride=1, padding=0),
@@ -29,7 +27,7 @@ class ResConvGenerator(nn.Module):
             nn.ReLU(True),
             # out size. (cnst.GAN_GEN_FEATURE_MAPS*4) x 7 x 7
             # ---------
-            nn.Upsample(scale_factor=2, mode='bilinear'),
+            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
             nn.ReflectionPad2d(1),
             nn.Conv2d(4 * cnst.GAN_GEN_FEATURE_MAPS, 2 * cnst.GAN_GEN_FEATURE_MAPS,
                       kernel_size=3, stride=1, padding=0),
@@ -37,7 +35,7 @@ class ResConvGenerator(nn.Module):
             nn.ReLU(True),
             # out size. (cnst.GAN_GEN_FEATURE_MAPS*2) x 14 x 14
             # ---------
-            nn.Upsample(scale_factor=2, mode='bilinear'),
+            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
             nn.ReflectionPad2d(1),
             nn.Conv2d(2 * cnst.GAN_GEN_FEATURE_MAPS, cnst.CHANNELS_NUM,
                       kernel_size=3, stride=1, padding=0),
@@ -46,9 +44,13 @@ class ResConvGenerator(nn.Module):
             # ---------
         )
 
+    def preprocess(self, noise, labels):
+        n_labels = self.label_embedding(labels)
+        n_labels = n_labels.reshape(n_labels.size(0), n_labels.size(1), 1, 1)
+        n_noise = torch.cat((n_labels, noise), 1)
+        return n_noise
+
     def forward(self, noise, labels):
-        labels = self.label_embedding(labels)
-        labels = labels.reshape(labels.size(0), labels.size(1), 1, 1)
-        noise = torch.cat((labels, noise), 1)
-        img = self.model(noise)
+        n_noise = self.preprocess(noise, labels)
+        img = self.model(n_noise)
         return img
