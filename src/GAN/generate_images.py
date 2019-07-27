e4141bdfa3ac6f8df9e3f6f641e9bd9fcc55e441
Daniel Matwicki <jkeezuz@gmail.com>
2019-07-27 17:22:59 +0200

    Generator now runs without CUDA because the program run out of CUDA memory while trying to generate 10K images. Downside is that it's way slower, but hey - it works, okay?

--- src/GAN/generate_images.py
+++ src/GAN/generate_images.py
@@ -17,26 +17,25 @@ if not os.path.exists(cnst.GAN_SAVE_DIR):
 n_classess = 10
 
 # Import MNIST dataset
-G = Generator(img_shape=(28, 28), n_classes=n_classess, latent_dim=cnst.GAN_LATENT_SIZE).cuda()
+G = Generator(n_classes=n_classess, latent_dim=cnst.GAN_LATENT_SIZE)
 FloatTensor = torch.cuda.FloatTensor
 LongTensor = torch.cuda.LongTensor
 
-G.load_state_dict(torch.load('G.ckpt'))
+G.load_state_dict(torch.load('G.ckpt', map_location='cpu'))
 
-num_of_images = 2
-z = FloatTensor(np.random.normal(0, 1, (num_of_images, cnst.GAN_LATENT_SIZE)))
+num_of_images_per_class = 2
+z = FloatTensor(np.random.normal(0, 1, (num_of_images_per_class, cnst.GAN_LATENT_SIZE)))
 # Get labels ranging from 0 to n_classes for n rows
-labels = np.array([1 for x in range(num_of_images)])
-
-available_labels = [0,3,4,6,9]
-single_label_imgs_num = 10
-z = torch.randn(len(available_labels) * single_label_imgs_num, cnst.GAN_LATENT_SIZE, 1, 1).cuda()
+available_labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+single_label_imgs_num = 1000
+z = torch.randn(len(available_labels) * single_label_imgs_num, cnst.GAN_LATENT_SIZE, 1, 1)
 # Get labels ranging from 0 to n_classes for n rows
 
 labels = np.array([label for label in available_labels for _ in range(single_label_imgs_num)])
-labels = torch.cuda.LongTensor(labels)
+labels = torch.LongTensor(labels)
 
 gen_imgs = G(z, labels)
+G.eval()
 
 save_image(gen_imgs.data, os.path.join(cnst.GAN_SAMPLES_DIR, "generatedVIS3.png"), nrow=n_classess, normalize=True)
 save_image(gen_imgs.data, os.path.join(cnst.GAN_SAMPLES_DIR, "generatedVIS4.png"), nrow=n_classess * 10, normalize=True)
@@ -47,9 +46,10 @@ labels = labels.cpu().data.numpy()
 np.save(file=os.path.join(cnst.GAN_DATA_DIR, "gan_images.npy"), arr=gen_imgs)
 np.save(file=os.path.join(cnst.GAN_DATA_DIR, "gan_labels.npy"), arr=labels)
 
+cpu_imgs = np.squeeze(gen_imgs)
 for i, label in enumerate(labels):
     plt.title('Label is {label}'.format(label=label))
-    plt.imshow(gen_imgs[i], cmap='gray')
+    plt.imshow(cpu_imgs[i], cmap='gray')
     plt.show()
     if i > 5:
         break
